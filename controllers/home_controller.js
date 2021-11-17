1455ce11a449de59ac531cfc75c54a9c72a7e736
aakankshabaijal <f20190304@goa.bits-pilani.ac.in>
2021-11-18 02:17:12 +0530

    Add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = (req, res) => {
+	return res.send('<h1>Express server is running for Instacode</h1>');
+};
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8000;
 //port 80 is for production/deployment
 
+//for using express router
+app.use('/', require('./routes/index'));
+
 app.listen(port, (err) => {
 	if (err) {
 		console.log(`Error in running the server : ${err}`);
