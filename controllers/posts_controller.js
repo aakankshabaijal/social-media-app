d8ba6b76b192d346b06ab04ef74e40b1f04e2204
aakankshabaijal <f20190304@goa.bits-pilani.ac.in>
2022-01-03 13:35:19 +0530

    Add view for posts in feed

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,11 @@
 const userPost = (req, res) => {
-	res.send('<h1>Here are the posts</h1>');
+	//res.send('<h1>Here are the posts</h1>');
+	res.render('posts', {
+		title    : 'Feed',
+		userName : 'aakanksha_baijal',
+		likes    : '10',
+		comments : '5'
+	});
 };
 
 module.exports = { userPost };
